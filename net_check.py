114b83d5fe725c0d6a0f03fbe3b03f5e4fef8930
George Kadas <kadas.george@yahoo.com>
2016-03-07 13:05:09 +0200

    Network Folder Check

---
+++ net_check.py
@@ -0,0 +1,22 @@
+import time
+from win_unc import (DiskDrive, UncDirectory,
+                     UncDirectoryConnection, UncDirectoryMount)
+
+# Connect a shared directory without authorization.
+unc = UncDirectory(r'\\atlas\Internal_Auditing')
+conn = UncDirectoryConnection(unc, persistent=True)
+
+while (True):
+    conn.connect()
+
+    if (conn.is_connected() == False):
+        print '...'
+        conn.disconnect()
+        time.sleep(60)
+    elif (conn.is_connected()== True):
+        print 'Connected?', conn.is_connected()
+        break
+	else:
+		print 'You should never see this assuming the script works correctly'
+
+
